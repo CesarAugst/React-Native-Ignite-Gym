61fcdea79e6d8b8d7e94e7bdf1ee3214faec0552
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-01-05 13:25:05 -0300

    foto do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,44 @@
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text } from 'native-base';
+
+import { UserPhoto } from '@components/UserPhoto';
+import { ScreenHeader } from '@components/ScreenHeader';
+
+const PHOTO_SIZE = 33;
 
 export function Profile(){
+    const [photoIsLoading, setPhotoIsLoading] = useState(false);
     return(
-        <Center flex={1}>
-            <Text color="white">Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader 
+                title='Perfil'
+            />
+            <ScrollView>
+                <Center mt={6} px={10}>
+                    {
+                        photoIsLoading ?
+                        <Skeleton 
+                            w={PHOTO_SIZE} 
+                            h={PHOTO_SIZE} 
+                            rounded="full"
+                            startColor="gray.500"
+                            endColor="gray.400"
+                        />
+                        :
+                        <UserPhoto 
+                            source={{ uri: 'https://github.com/cesaraugst.png'}}
+                            alt="Foto do usuario"
+                            size={PHOTO_SIZE}
+                        />
+                    }
+                    <TouchableOpacity>
+                        <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+                            Alterar foto
+                        </Text>
+                    </TouchableOpacity>
+                </Center>
+            </ScrollView>
+        </VStack>
     );
 }
