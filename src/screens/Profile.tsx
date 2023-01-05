4866f109c57758239d96c1dfccc3c04ed5ef6f9a
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-01-05 13:42:15 -0300

    finalizando tela de perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -9,30 +9,30 @@ import { Input } from '@components/Input';
 
 const PHOTO_SIZE = 33;
 
-export function Profile(){
+export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
-    return(
+    return (
         <VStack flex={1}>
-            <ScreenHeader 
+            <ScreenHeader
                 title='Perfil'
             />
-            <ScrollView>
+            <ScrollView contentContainerStyle={{ paddingBottom: 56 }} >
                 <Center mt={6} px={10}>
                     {
                         photoIsLoading ?
-                        <Skeleton 
-                            w={PHOTO_SIZE} 
-                            h={PHOTO_SIZE} 
-                            rounded="full"
-                            startColor="gray.500"
-                            endColor="gray.400"
-                        />
-                        :
-                        <UserPhoto 
-                            source={{ uri: 'https://github.com/cesaraugst.png'}}
-                            alt="Foto do usuario"
-                            size={PHOTO_SIZE}
-                        />
+                            <Skeleton
+                                w={PHOTO_SIZE}
+                                h={PHOTO_SIZE}
+                                rounded="full"
+                                startColor="gray.500"
+                                endColor="gray.400"
+                            />
+                            :
+                            <UserPhoto
+                                source={{ uri: 'https://github.com/cesaraugst.png' }}
+                                alt="Foto do usuario"
+                                size={PHOTO_SIZE}
+                            />
                     }
                     <TouchableOpacity>
                         <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
@@ -40,40 +40,39 @@ export function Profile(){
                         </Text>
                     </TouchableOpacity>
 
-                    <Input 
+                    <Input
                         placeholder='Nome'
                         bg="gray.600"
                     />
-                    <Input 
+                    <Input
                         placeholder='E-mail'
                         bg="gray.600"
                         isDisabled
                     />
-                </Center>
-                <VStack px={10} mt={12} mb={9}>
-                    <Heading color="gray.200" fontSize="md" mb={2}>
+
+                    <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12}>
                         Alterar senha
                     </Heading>
-                    <Input 
+                    <Input
                         bg="gray.600"
                         placeholder='Senha Antiga'
                         secureTextEntry
                     />
-                    <Input 
+                    <Input
                         bg="gray.600"
                         placeholder='Nova Antiga'
                         secureTextEntry
                     />
-                    <Input 
+                    <Input
                         bg="gray.600"
                         placeholder='Confirme a Senha Antiga'
                         secureTextEntry
                     />
-                    <Button 
+                    <Button
                         title='Atulizar'
                         mt={4}
                     />
-                </VStack>
+                </Center>
             </ScrollView>
         </VStack>
     );
