f9cc1e046bf1eaea250161dbc190a63916aa34b7
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-01-09 11:54:11 -0300

    armazenamento dos dados do usuario logado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,9 @@
-import { UserDTO } from "@dtos/UserDTO";
 import { createContext, ReactNode, useState } from "react";
+
+import { storageUserSave } from "@storage/storageUser";
+
 import { api } from '@services/api';
+import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
     user: UserDTO;
@@ -22,6 +25,7 @@ export function AuthContextProvider({children}: AuthContextProviderProps) {
 
             if(data.user){
                 setUser(data.user);
+                storageUserSave(data.user);
             } 
         } catch (error) {
             throw error;
